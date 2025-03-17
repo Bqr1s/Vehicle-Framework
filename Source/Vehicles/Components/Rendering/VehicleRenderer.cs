bb87242154b235018adbf48d7644117530bfd953
Phil <supersmashphil@gmail.com>
2025-03-17 17:21:10 -0600

    renderer fix

--- Source/Vehicles/Components/Rendering/VehicleRenderer.cs
+++ Source/Vehicles/Components/Rendering/VehicleRenderer.cs
@@ -8,106 +8,126 @@ using System.Security.Cryptography;
 
 namespace Vehicles
 {
-	public sealed class VehicleRenderer
-	{
-		// values pulled from RimWorld pawn offsets
-		public const float SubInterval = 0.003787879f;
-		public const float YOffset_Body = 0.007575758f;
-		public const float YOffset_Damage = 0.018939395f;
-		public const float YOffset_CoveredInOverlay = 0.033301156f;
+  public sealed class VehicleRenderer
+  {
+    // values pulled from RimWorld pawn offsets
+    public const float SubInterval = 0.003787879f;
+    public const float YOffset_Body = 0.007575758f;
+    public const float YOffset_Damage = 0.018939395f;
+    public const float YOffset_CoveredInOverlay = 0.033301156f;
 
-		private readonly VehiclePawn vehicle;
+    private readonly VehiclePawn vehicle;
 
-		//public VehicleGraphicSet graphics;
+    //public VehicleGraphicSet graphics;
 
-		//private Graphic_DynamicShadow shadowGraphic;
+    //private Graphic_DynamicShadow shadowGraphic;
 
-		//private PawnFirefoamDrawer firefoamOverlays;
+    //private PawnFirefoamDrawer firefoamOverlays;
 
-		public VehicleRenderer(VehiclePawn vehicle)
-		{
-			this.vehicle = vehicle;
-			//graphics = new VehicleGraphicSet(vehicle);
+    public VehicleRenderer(VehiclePawn vehicle)
+    {
+      this.vehicle = vehicle;
+      //graphics = new VehicleGraphicSet(vehicle);
 
-			//firefoamOverlays = new PawnFirefoamDrawer(vehicle);
-		}
+      //firefoamOverlays = new PawnFirefoamDrawer(vehicle);
+    }
 
-		[Obsolete("Not currently implemented, still WIP. Do not reference.", error: true)]
-		public PawnFirefoamDrawer FirefoamOverlays => throw new NotImplementedException();// firefoamOverlays;
+    [Obsolete("Not currently implemented, still WIP. Do not reference.", error: true)]
+    public PawnFirefoamDrawer FirefoamOverlays =>
+      throw new NotImplementedException(); // firefoamOverlays;
 
-		// TODO 1.6 - Rename to RenderVehicleAt
-		// Mark That Pawn and Camera+ have patches on this method
-		[Obsolete]
-		public void RenderPawnAt(Vector3 drawLoc, float extraRotation, bool northSouthRotation)
-		{
-			TransformData transform = new(drawLoc, vehicle.FullRotation, extraRotation);
-			RenderVehicle(in transform);
-		}
+    // TODO 1.6 - Rename to RenderVehicleAt
+    // Mark That Pawn and Camera+ have patches on this method
+    [Obsolete]
+    public void RenderPawnAt(Vector3 drawLoc, float extraRotation, bool northSouthRotation)
+    {
+      TransformData transform = new(drawLoc, vehicle.FullRotation, extraRotation);
+      RenderVehicle(in transform);
+    }
 
-		public void RenderVehicle(ref readonly TransformData transform)
-		{
-			RenderVehicleWithOverlays(in transform);
-			vehicle.VehicleGraphic.ShadowGraphic?.Draw(transform.position, vehicle.FullRotation, vehicle, 0f);
-			if (vehicle.Spawned && !vehicle.Dead)
-			{
-				vehicle.vehiclePather.PatherDraw();
-			}
-		}
+    public void RenderVehicle(ref readonly TransformData transform)
+    {
+      RenderVehicleWithOverlays(in transform);
+      vehicle.VehicleGraphic.ShadowGraphic?.Draw(transform.position, vehicle.FullRotation, vehicle,
+        0f);
+      if (vehicle.Spawned && !vehicle.Dead)
+      {
+        vehicle.vehiclePather.PatherDraw();
+      }
+    }
 
-		// Vehicle body rendering + overlays
-		[Obsolete]
-		private void RenderVehicle(Vector3 rootLoc, Rot8 rot, float extraRotation, bool northSouthRotation)
-		{
-			if (vehicle.Spawned)
-			{
-				vehicle.UpdateRotationAndAngle();
-			}
-			TransformData transform = new(rootLoc, rot, extraRotation);
-			RenderVehicleWithOverlays(in transform);
-		}
+    // Vehicle body rendering + overlays
+    [Obsolete("Use new transform based rendering instead.", error: true)]
+    public void RenderPawnAt_TEMP(Vector3 drawLoc, Rot8 rot, float extraRotation,
+      bool northSouthRotation)
+    {
+      if (vehicle.Spawned)
+      {
+        vehicle.UpdateRotationAndAngle();
+      }
 
-		private void RenderVehicleWithOverlays(ref readonly TransformData transform)
-		{
-			// TODO - needs to add aboveBodyPos to transform
-			Vector3 aboveBodyPos = RenderVehicleInternal(in transform);
-			vehicle.DrawExplosiveWicks(aboveBodyPos, transform.orientation);
-			vehicle.overlayRenderer.DrawOverlays(in transform);
-		}
+      TransformData transform = new(drawLoc, rot, extraRotation);
+      RenderVehicleWithOverlays(in transform);
+    }
 
-		// Vehicle body rendering
-		private Vector3 RenderVehicleInternal(ref readonly TransformData transform)
-		{
-			Quaternion quaternion = Quaternion.AngleAxis((transform.orientation.AsRotationAngle + transform.rotation) * 
-				(vehicle.NorthSouthRotation ? -1 : 1), Vector3.up);
+    // Vehicle body rendering + overlays
+    [Obsolete("Use new transform based rendering instead.", error: true)]
+    private void RenderVehicle(Vector3 rootLoc, Rot8 rot, float extraRotation,
+      bool northSouthRotation)
+    {
+      if (vehicle.Spawned)
+      {
+        vehicle.UpdateRotationAndAngle();
+      }
 
-			Vector3 aboveBodyPos = transform.position + vehicle.VehicleGraphic.DrawOffset(transform.orientation);
-			aboveBodyPos.y += YOffset_Body;
-			Mesh mesh = vehicle.VehicleGraphic.MeshAtFull(transform.orientation);
-			Material material = vehicle.VehicleGraphic.MatAtFull(transform.orientation);
+      TransformData transform = new(rootLoc, rot, extraRotation);
+      RenderVehicleWithOverlays(in transform);
+    }
 
-			GenDraw.DrawMeshNowOrLater(mesh, aboveBodyPos, quaternion, material, false);
-			aboveBodyPos.y += SubInterval;
+    private void RenderVehicleWithOverlays(ref readonly TransformData transform)
+    {
+      // TODO - needs to add aboveBodyPos to transform
+      Vector3 aboveBodyPos = RenderVehicleInternal(in transform);
+      vehicle.DrawExplosiveWicks(aboveBodyPos, transform.orientation);
+      vehicle.overlayRenderer.DrawOverlays(in transform);
+    }
 
-			Vector3 drawLoc = transform.position;
-			drawLoc.y += YOffset_Damage;
-			// TODO - Firefoam overlays for vehicle
-			//if (firefoamOverlays.IsCoveredInFoam)
-			//{
-			//	Vector3 overlayPos = rootLoc;
-			//	overlayPos.y += YOffset_CoveredInOverlay;
-			//	firefoamOverlays.RenderPawnOverlay(overlayPos, mesh, quaternion, flags.FlagSet(PawnRenderFlags.DrawNow), PawnOverlayDrawer.OverlayLayer.Body, bodyFacing);
-			//}
+    // Vehicle body rendering
+    private Vector3 RenderVehicleInternal(ref readonly TransformData transform)
+    {
+      Quaternion quaternion = Quaternion.AngleAxis(
+        (transform.orientation.AsRotationAngle + transform.rotation) *
+        (vehicle.NorthSouthRotation ? -1 : 1), Vector3.up);
 
-			//TODO - pack graphics
-			//if (vehicle.inventory != null && vehicle.inventory.innerContainer.Count > 0 && graphics.packGraphic != null)
-			//{
-			//	Graphics.DrawMesh(mesh, drawLoc, quaternion, graphics.packGraphic.MatAt(bodyFacing, null), 0);
-			//}
-			return aboveBodyPos;
-		}
+      Vector3 aboveBodyPos =
+        transform.position + vehicle.VehicleGraphic.DrawOffset(transform.orientation);
+      aboveBodyPos.y += YOffset_Body;
+      Mesh mesh = vehicle.VehicleGraphic.MeshAtFull(transform.orientation);
+      Material material = vehicle.VehicleGraphic.MatAtFull(transform.orientation);
 
-		public void ProcessPostTickVisuals(int ticksPassed)
-		{
-		}
-	}
+      GenDraw.DrawMeshNowOrLater(mesh, aboveBodyPos, quaternion, material, false);
+      aboveBodyPos.y += SubInterval;
+
+      Vector3 drawLoc = transform.position;
+      drawLoc.y += YOffset_Damage;
+      // TODO - Firefoam overlays for vehicle
+      //if (firefoamOverlays.IsCoveredInFoam)
+      //{
+      //	Vector3 overlayPos = rootLoc;
+      //	overlayPos.y += YOffset_CoveredInOverlay;
+      //	firefoamOverlays.RenderPawnOverlay(overlayPos, mesh, quaternion, flags.FlagSet(PawnRenderFlags.DrawNow), PawnOverlayDrawer.OverlayLayer.Body, bodyFacing);
+      //}
+
+      //TODO - pack graphics
+      //if (vehicle.inventory != null && vehicle.inventory.innerContainer.Count > 0 && graphics.packGraphic != null)
+      //{
+      //	Graphics.DrawMesh(mesh, drawLoc, quaternion, graphics.packGraphic.MatAt(bodyFacing, null), 0);
+      //}
+      return aboveBodyPos;
+    }
+
+    public void ProcessPostTickVisuals(int ticksPassed)
+    {
+    }
+  }
 }
