1c8a566e64a2cece5eac5069d470db9f6cac9e3d
Phil <supersmashphil@gmail.com>
2025-03-17 16:32:43 -0600

    added files for prev commit

---
+++ Source/Vehicles/Harmony/UnitTesting/UnitTestDeferredGeneration.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Threading;
+using SmashTools;
+using SmashTools.Debugging;
+using SmashTools.Performance;
+using UnityEngine;
+using Verse;
+
+namespace Vehicles.Testing
+{
+  internal class UnitTestDeferredGeneration : UnitTestMapTest
+  {
+    private const double MaxWaitTime = 5000; // ms
+
+    public override string Name => "DeferredGridGeneration";
+
+    protected override bool ShouldTest(VehicleDef vehicleDef)
+    {
+      return PathingHelper.ShouldCreateRegions(vehicleDef) && GridOwners.IsOwner(vehicleDef);
+    }
+
+    protected override UTResult TestVehicle(VehiclePawn vehicle, IntVec3 root)
+    {
+      VehicleDef vehicleDef = vehicle.VehicleDef;
+      int maxSize = Mathf.Max(vehicleDef.Size.x, vehicleDef.Size.z);
+
+      UTResult result = new();
+
+      VehicleMapping mapping = TestMap.GetCachedMapComponent<VehicleMapping>();
+      VehicleMapping.VehiclePathData pathData = mapping[vehicleDef];
+      VehicleRegionGrid regionGrid = pathData.VehicleRegionGrid;
+      VehiclePathGrid pathGrid = pathData.VehiclePathGrid;
+
+      if (mapping.deferredGridGeneration == null || !mapping.ThreadAlive)
+        return UTResult.For("DeferredGeneration", UTResult.Result.Skipped);
+
+      mapping.deferredGridGeneration.ReleaseAll(vehicleDef);
+      Assert.IsTrue(pathData.Suspended);
+
+      // We're specifically testing thread enqueueing with deferred grid generation, we need the 
+      // dedicated thread available in order to test this. Will return to suspended after test
+      // since we're running this as a UnitTestMapTest.
+      using ThreadEnabler te = new();
+      ManualResetEventSlim mres = new(false);
+
+      GenSpawn.Spawn(vehicle, root, TestMap);
+      // Faction.OfPlayer
+      result.Add($"DeferredGeneration_{vehicleDef} (Spawned)", vehicle.Spawned);
+      result.Add($"DeferredGeneration (Player Deferred)",
+        DeferredGridGeneration.UrgencyFor(vehicle) == DeferredGridGeneration.Urgency.Deferred);
+
+      // We need to wait for the dedicated thread to finish generating vehicle's grids so we can
+      // validate that every grid is initialized.
+      AsyncLongOperationAction longOp = AsyncPool<AsyncLongOperationAction>.Get();
+      longOp.Set(() => NotifyReadyToContinue(mres));
+      mapping.dedicatedThread.Enqueue(longOp);
+      mres.Wait(TimeSpan.FromMilliseconds(MaxWaitTime));
+
+      result.Add($"DeferredGeneration (Player Regions Generated)",
+        pathData.VehicleRegionAndRoomUpdater.Enabled);
+      result.Add($"DeferredGeneration (Player PathGrid Generated)",
+        pathData.VehiclePathGrid.Enabled);
+      result.Add($"DeferredGeneration (Player PathData Status)", !pathData.Suspended);
+
+      vehicle.DeSpawn();
+      Assert.IsTrue(!vehicle.Spawned);
+      mapping.deferredGridGeneration.ReleaseAll(vehicleDef);
+      Assert.IsTrue(pathData.Suspended);
+
+      // Block dedicated thread without flagging as suspended so we can still validate that
+      // grid generation is not being sent to the dedicated thread for deferred generation of
+      // map grids. This is equivalent to clogging up the dedicated thread until we decide we're
+      // ready or we hit the timeout threshold.
+      mres.Reset();
+      AsyncLongOperationAction blockingOp = AsyncPool<AsyncLongOperationAction>.Get();
+      blockingOp.Set(() => WaitForSignal(mres));
+      mapping.dedicatedThread.Enqueue(blockingOp);
+
+      Assert.IsNotNull(Find.World.factionManager.OfAncientsHostile);
+      vehicle.SetFactionDirect(Find.World.factionManager.OfAncientsHostile);
+      GenSpawn.Spawn(vehicle, root, TestMap);
+      result.Add($"DeferredGeneration_{vehicleDef} (Spawned Enemy)", vehicle.Spawned);
+      result.Add($"DeferredGeneration (Enemy Urgent)",
+        DeferredGridGeneration.UrgencyFor(vehicle) == DeferredGridGeneration.Urgency.Urgent);
+
+      result.Add($"DeferredGeneration (Enemy Regions Generated)",
+        pathData.VehicleRegionAndRoomUpdater.Enabled);
+      result.Add($"DeferredGeneration (Enemy PathGrid Generated)",
+        pathData.VehiclePathGrid.Enabled);
+      result.Add($"DeferredGeneration (Enemy PathData Status)", !pathData.Suspended);
+
+      // Unblock dedicated thread
+      mres.Set();
+
+      return result;
+    }
+
+    private static void WaitForSignal(ManualResetEventSlim mre)
+    {
+      mre.Wait(TimeSpan.FromMilliseconds(MaxWaitTime));
+    }
+
+    private static void NotifyReadyToContinue(ManualResetEventSlim mre)
+    {
+      mre.Set();
+    }
+  }
+}
---
+++ Source/Vehicles/Pathing/Map/DeferredGridGeneration.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using LudeonTK;
+using RimWorld;
+using SmashTools;
+using SmashTools.Performance;
+using Verse;
+
+namespace Vehicles;
+
+public class DeferredGridGeneration
+{
+  private const int DaysUnusedForRemoval = 3;
+
+  private readonly VehicleMapping mapping;
+
+  private readonly GridCounter regionGrid = new();
+  private readonly GridCounter pathGrid = new();
+
+  public DeferredGridGeneration(VehicleMapping mapping)
+  {
+    this.mapping = mapping;
+  }
+
+  internal void GenerateAllPathGrids()
+  {
+    foreach (VehicleDef vehicleDef in DefDatabase<VehicleDef>.AllDefsListForReading)
+    {
+      Action postGenerationCallback = null;
+#if DEBUG
+      postGenerationCallback = () => CoroutineManager.QueueInvoke(() =>
+        Ext_Messages.Message($"PathGrid generated for vehicle {vehicleDef.LabelCap}.",
+          MessageTypeDefOf.SilentInput,
+          time: 0.5f, historical: false));
+#endif
+      RequestPathGrid(vehicleDef, Urgency.Deferred,
+        postGenerationCallback: postGenerationCallback);
+    }
+  }
+
+  internal void GenerateAllRegionGrids()
+  {
+    foreach (VehicleDef ownerDef in GridOwners.AllOwners)
+    {
+      Action postGenerationCallback = null;
+#if DEBUG
+      postGenerationCallback = () => CoroutineManager.QueueInvoke(() =>
+        Ext_Messages.Message($"Regions generated for owner {ownerDef.LabelCap}.",
+          MessageTypeDefOf.SilentInput,
+          time: 0.5f, historical: false));
+#endif
+      RequestRegionSet(ownerDef, Urgency.Deferred,
+        postGenerationCallback: postGenerationCallback);
+    }
+  }
+
+  public void RequestGridsFor(VehicleDef vehicleDef, Urgency urgency,
+    Action postGenerationCallback = null)
+  {
+    if (RequestPathGrid(vehicleDef, urgency, postGenerationCallback) == Urgency.Urgent)
+      Debug.Message($"Skipped deferred PathGrid generation for {vehicleDef}");
+    if (RequestRegionSet(vehicleDef, urgency, postGenerationCallback) == Urgency.Urgent)
+      Debug.Message($"Skipped deferred Region generation for {vehicleDef}");
+  }
+
+  public void DoPass()
+  {
+    Assert.IsTrue(regionGrid.UsedCount == 0);
+    Assert.IsTrue(pathGrid.UsedCount == 0);
+    foreach (Pawn pawn in mapping.map.mapPawns.AllPawns)
+    {
+      if (pawn is VehiclePawn vehicle)
+      {
+        pathGrid.SetUsed(vehicle.VehicleDef);
+        regionGrid.SetUsed(GridOwners.GetOwner(vehicle.VehicleDef));
+      }
+    }
+
+    foreach (Pawn pawn in Find.World.worldPawns.AllPawnsAlive)
+    {
+      if (pawn is VehiclePawn vehicle && vehicle.Faction.IsPlayerSafe())
+      {
+        pathGrid.SetUsed(vehicle.VehicleDef);
+        regionGrid.SetUsed(GridOwners.GetOwner(vehicle.VehicleDef));
+      }
+    }
+
+    // Release region set for vehicles not actively in use
+    foreach (VehicleDef vehicleDef in DefDatabase<VehicleDef>.AllDefsListForReading)
+    {
+      if (GridOwners.IsOwner(vehicleDef) && regionGrid.IsUsed(vehicleDef))
+        continue;
+
+      if (pathGrid.IsUsed(vehicleDef))
+        continue;
+
+      pathGrid.IncrementUnused(vehicleDef);
+      // Remove when exceeds days unused
+      if (pathGrid.ShouldRemoveGrid(vehicleDef))
+      {
+        ReleasePathGrid(vehicleDef);
+      }
+
+      if (GridOwners.IsOwner(vehicleDef))
+      {
+        regionGrid.IncrementUnused(vehicleDef);
+        if (regionGrid.ShouldRemoveGrid(vehicleDef))
+        {
+          ReleaseRegionSet(vehicleDef);
+        }
+      }
+    }
+
+    pathGrid.OnPassDone();
+    regionGrid.OnPassDone();
+  }
+
+  private Urgency RequestPathGrid(VehicleDef vehicleDef, Urgency urgency,
+    Action postGenerationCallback = null)
+  {
+    VehicleMapping.VehiclePathData pathData = mapping[vehicleDef];
+    // Path grid has already been initialized
+    if (pathData.VehiclePathGrid.Enabled)
+    {
+      postGenerationCallback?.Invoke();
+      return Urgency.None;
+    }
+
+    // If thread is not available, all rebuild requests are urgent
+    if (urgency < Urgency.Urgent && mapping.ThreadAlive)
+    {
+      AsyncLongOperationAction longOperation = AsyncPool<AsyncLongOperationAction>.Get();
+      longOperation.Set(GeneratePathGrid, () => !mapping.map.Disposed);
+      mapping.dedicatedThread.Enqueue(longOperation);
+      return Urgency.Deferred;
+    }
+
+    GeneratePathGrid();
+    return Urgency.Urgent;
+
+    void GeneratePathGrid()
+    {
+      if (pathData.VehiclePathGrid.Enabled) return;
+
+      pathData.VehiclePathGrid.RecalculateAllPerceivedPathCosts();
+
+      // Post-generation event should be invoked on the main thread
+      if (postGenerationCallback != null)
+      {
+        CoroutineManager.QueueInvoke(postGenerationCallback);
+      }
+    }
+  }
+
+  private Urgency RequestRegionSet(VehicleDef vehicleDef, Urgency urgency,
+    Action postGenerationCallback = null)
+  {
+    VehicleDef ownerDef = GridOwners.GetOwner(vehicleDef);
+    VehicleMapping.VehiclePathData pathData = mapping[ownerDef];
+    // Region grid has already been initialized
+    if (!pathData.Suspended)
+    {
+      postGenerationCallback?.Invoke();
+      return Urgency.None;
+    }
+
+    // If thread is not available, all rebuild requests are urgent
+    if (urgency < Urgency.Urgent && mapping.ThreadAlive)
+    {
+      AsyncLongOperationAction longOperation = AsyncPool<AsyncLongOperationAction>.Get();
+      longOperation.Set(GenerateRegions, () => !mapping.map.Disposed);
+      mapping.dedicatedThread.Enqueue(longOperation);
+      return Urgency.Deferred;
+    }
+
+    GenerateRegions();
+    return Urgency.Urgent;
+
+    void GenerateRegions()
+    {
+      if (!pathData.Suspended) return;
+
+      pathData.VehicleRegionAndRoomUpdater.Init();
+      pathData.VehicleRegionAndRoomUpdater.RebuildAllVehicleRegions();
+      // post-generation event should be invoked on the main thread
+      if (postGenerationCallback != null)
+      {
+        CoroutineManager.QueueInvoke(postGenerationCallback);
+      }
+    }
+  }
+
+  internal void ReleaseAll(VehicleDef vehicleDef)
+  {
+    ReleasePathGrid(vehicleDef);
+    ReleaseRegionSet(GridOwners.GetOwner(vehicleDef));
+  }
+
+  private void ReleasePathGrid(VehicleDef ownerDef)
+  {
+    VehicleMapping.VehiclePathData pathData = mapping[ownerDef];
+    if (pathData.VehiclePathGrid.Enabled)
+    {
+      pathData.VehiclePathGrid.Release();
+
+#if DEBUG
+      Ext_Messages.Message($"Released PathGrid for {ownerDef}", MessageTypeDefOf.SilentInput,
+        time: 1f, historical: false);
+#endif
+    }
+  }
+
+  private void ReleaseRegionSet(VehicleDef ownerDef)
+  {
+    VehicleMapping.VehiclePathData pathData = mapping[ownerDef];
+    if (!pathData.Suspended)
+    {
+      pathData.VehicleRegionAndRoomUpdater.Release();
+
+#if DEBUG
+      Ext_Messages.Message($"Released Regions for {ownerDef}", MessageTypeDefOf.SilentInput,
+        time: 1f, historical: false);
+#endif
+    }
+  }
+
+  public static Urgency UrgencyFor(VehiclePawn vehicle)
+  {
+    // If null faction, vehicle is immovable anyways
+    if (vehicle.Faction == null) return Urgency.None;
+
+    if (!vehicle.Faction.IsPlayer)
+    {
+      // Non-player factions need grid urgently for incidents
+      return Urgency.Urgent;
+    }
+
+    return Urgency.Deferred;
+  }
+
+  [DebugAction(VehicleHarmony.VehiclesLabel, "Force Remove Unused Regions")]
+  private static void DoPassOnAllMaps()
+  {
+    // Simulate multiple days for multi-pass based removal
+    for (int i = 0; i < DaysUnusedForRemoval; i++)
+    {
+      foreach (Map map in Find.Maps)
+      {
+        VehicleMapping mapping = map.GetCachedMapComponent<VehicleMapping>();
+        mapping.deferredGridGeneration.DoPass();
+      }
+    }
+  }
+
+  private class GridCounter
+  {
+    private readonly Dictionary<VehicleDef, int> countdownToRemoval = [];
+    private readonly HashSet<VehicleDef> activelyUsed = [];
+
+    public int UsedCount => activelyUsed.Count;
+
+    public bool IsUsed(VehicleDef vehicleDef)
+    {
+      return activelyUsed.Contains(vehicleDef);
+    }
+
+    public void SetUsed(VehicleDef vehicleDef)
+    {
+      activelyUsed.Add(vehicleDef);
+      countdownToRemoval.Remove(vehicleDef);
+    }
+
+    public void IncrementUnused(VehicleDef vehicleDef)
+    {
+      if (!countdownToRemoval.ContainsKey(vehicleDef))
+        countdownToRemoval.Add(vehicleDef, 0);
+      countdownToRemoval[vehicleDef]++;
+    }
+
+    /// <returns>VehicleDef grid has been unused past threshold and can be removed.</returns>
+    public bool ShouldRemoveGrid(VehicleDef vehicleDef)
+    {
+      // Remove when exceeds days unused
+      return countdownToRemoval[vehicleDef] >= DaysUnusedForRemoval;
+    }
+
+    public void OnPassDone()
+    {
+      activelyUsed.Clear();
+    }
+  }
+
+  public enum Urgency
+  {
+    None,
+    Deferred, // Queue request to thread
+    Urgent, // Generate immediately
+  }
+}
